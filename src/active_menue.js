a69dddf5da95a3923209a6e84dc4c6b892ad44b3
generation21 <qbration21@gmail.com>
2023-06-24 22:07:34 +0900

    JS-Observe

---
+++ src/active_menue.js
@@ -0,0 +1,57 @@
+// 구현 계획
+// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 사용해서 모든 섹션을 관찰해야한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+// 보여지는 섹션:
+// - 다수의 섹션이 동시에 보여진다면, 가장 상위에 있는 것을 보여준다.
+// - but Footer는 Contact을 활성화
+
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonial",
+  "#contact",
+];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = { rootMargin: "-20% 0px 0px 0px", threshold: [0, 0.99] };
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+  let selectLastOne;
+  entries.forEach((entry) => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
+  });
+
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
+}
+function findFirstIntersecting(visibleSections) {
+  const index = visibleSections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
+}
