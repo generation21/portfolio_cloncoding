11b48531cce7e1d09daa0d05648dc105e66aec66
generation21 <qbration21@gmail.com>
2023-06-24 16:05:09 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector(".header");
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
@@ -8,4 +8,13 @@ document.addEventListener("scroll", () => {
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+
+document.addEventListener("scroll", () => {
+  const opacity = 1 - window.scrollY / homeHeight;
+  home.style.opacity = opacity;
 });
